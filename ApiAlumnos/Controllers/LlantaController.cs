411bf7fa8b9ab889ae45a17d2a6e6069c20d3cbf
LuisDiaz-ipsilon <99diazluisfernand@gmail.com>
2023-03-01 22:28:13 -0600

    fix-succes

--- ApiAlumnos/Controllers/LlantaController.cs
+++ ApiAlumnos/Controllers/LlantaController.cs
@@ -3,22 +3,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ApiFacturador.Controllers
 {
-    public class LlantaController
+
+
+    [ApiController]
+    [Route("api/llantas")]
+
+    public class LlantasController : ControllerBase
     {
-
-        [ApiController]
-        [Route("api/facturas")]
-
-        public class LlantasController: ControllerBase
+        [HttpGet]
+        public ActionResult<List<Llanta>> Get()
         {
-            [HttpGet]
-            public ActionResult<List<Llanta>> Get()
-            {
-                return new List<Llanta>()
+            return new List<Llanta>()
                 {
                     new Llanta(){id=1, tipoLlanta = "Racing goma" }
                 };
-            }
         }
     }
+
 }
--- ApiAlumnos/Controllers/MotorController.cs
+++ ApiAlumnos/Controllers/MotorController.cs
@@ -3,20 +3,19 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ApiFacturador.Controllers
 {
-    public class MotorController
+
+    [ApiController]
+    [Route("api/motores")]
+    public class MotoresController : ControllerBase
     {
-        [ApiController]
-        [Route("api/motores")]
-        public class MotoresController: ControllerBase
+        [HttpGet]
+        public ActionResult<List<Motor>> Get()
         {
-            [HttpGet]
-            public ActionResult<List<Motor>> Get()
-            {
-                return new List<Motor>() 
+            return new List<Motor>()
                 {
                     new Motor(){id = 1, tamaño = "600", demon = true, ethanol = true, nitro = true, supercharger = true, turbo = true}
                 };
-            }
         }
     }
+
 }
